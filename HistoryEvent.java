a7b6bef2a655aa9eabfa73d7a7f29e6a3cabae75
OwenMathews <owmathews@icloud.com>
2026-01-11 21:39:55 -0500

    Correct constructor and method name typos
    Fix constructor name and typos in getter methods.

---
+++ HistoryEvent.java
@@ -0,0 +1,28 @@
+public class HistoryEvent
+  {
+    private String keyword;
+    private String title;
+    private String description;
+
+    public istoryEvent(String keyword, String  title, String description)
+        {
+          this.keyword
+          this.title
+          this.description
+        }
+
+    public String getKeyword()
+    {
+      return keyword;
+    }
+
+    public String getTtile()
+    {
+      return title;
+    }
+
+    public String getDescription()
+    {
+      return description;
+    }
+  }
